3bda27c8345be7a31459cacf639665a42857f32f
suzygiv <suzannegivnish@gmail.com>
2020-11-21 10:41:05 -0500

    added public and assets folders

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', FALSE);
