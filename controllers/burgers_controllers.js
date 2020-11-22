bd163dbb56abd2555b936660d95d4d94ab10eb88
suzygiv <suzannegivnish@gmail.com>
2020-11-22 17:22:02 -0500

    updated all files

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,61 +1,44 @@
 var express = require("express");
-
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
-var cat = require("../models/burgers.js");
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  burgers.all(function(data) {
-    var hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
-  });
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
+        var hbsObject = {
+            burgers: data
+          };
+        res.render("index", hbsObject);
+    });
 });
 
-router.post("/api/burgers", function(req, res) {
-  burgers.create([
-    "name", "sleepy"
-  ], [
-    req.body.name, req.body.sleepy
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+// POST route which will be used to create a newBurger
+router.post("/api/burgers", function (req, res) {
+    burger.create(req.body.name, function (result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
   });
-});
-
-router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burgers.update({
-    devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+  
+  // PUT route which will update the boolean devoured
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    burger.update(
+      {
+        devoured : req.body.devoured
+      },
+      condition, function (result) {
+      if (result.changedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    })
   });
-});
+  
 
-router.delete("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  burgers.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
 
 // Export routes for server.js to use.
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,29 +1,22 @@
-// Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
-var burgers = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
-      cb(res);
-    });
-  }
+var burger = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+
+    insertOne: function (cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function (res) {
+            cb(res);
+        });
+    },
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    }
 };
 
-// Export the database functions for the controller (burgers_controllers.js).
-module.exports = burgers;
+module.exports = burger;
