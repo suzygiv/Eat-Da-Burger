52185e50fd70b3b5f13f4019e5fe8bc052613138
suzygiv <suzannegivnish@gmail.com>
2020-11-24 15:39:16 -0500

    connecting to jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 require('dotenv').config
 
 if (process.env.JAWSDB_URL) {
     // connect to JawsDB (heroku)
     connection = mysql.createConnection(process.env.JAWSDB_URL);
   } else {
-    
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: process.env.DB_PASSWORD,
-    database: "burgers_db"
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.DB_PASSWORD,
+        database: "burgers_db"
 });
 
 // Make connection.
